6aee072381788bf3a52f334464eac7103155bf4d
Baines Deal <bainesdeal@gmail.com>
2022-10-10 13:26:08 -0500

    Add project files.

---
+++ NewAPI/Controllers/ContactBookController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactBookController : ControllerBase
+    {
+        [HttpGet("GetContacts")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("CreateContact")]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("UpdateContact")]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteContact")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+    }
+}
